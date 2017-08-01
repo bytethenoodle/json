4942ea574b8f0f12ad17541f11e2a59d58e22d38
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-08-02 03:10:34 +0800

    updated defs

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ let package = Package(
     name: "JSON",
     dependencies: [
         // Core protocols, extensions, and functionality
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
+        .Package(url: "https://github.com/bytethenoodle/core.git", majorVersion: 2),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
+        .Package(url: "https://github.com/bytethenoodle/node.git", majorVersion: 2),
     ]
 )
